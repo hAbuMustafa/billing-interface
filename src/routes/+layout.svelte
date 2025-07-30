c3ea8213a117e9ff03f03c019ecf26185403b58a
hAbuMustafa <hosam5553@gmail.com>
2025-07-30 12:26:27 +0300

    تصحيح إملائي

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-  import { page } from "$app/state";
-  import Nav from "$lib/components/Nav.svelte";
-  import "./styles.css";
+  import { page } from '$app/state';
+  import Nav from '$lib/components/Nav.svelte';
+  import './styles.css';
   const { children } = $props();
 </script>
 
 <svelte:head>
-  <title>{page.data.title ? `${page.data.title} | ` : ""}صيدلية الإقتصادي</title
-  >
+  <title>{page.data.title ? `${page.data.title} | ` : ''}صيدلية الاقتصادي</title>
 </svelte:head>
 
 <Nav />
