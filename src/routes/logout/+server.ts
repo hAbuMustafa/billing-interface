df2eedc9be96b71d1fbc33e1890fe124299cacfb
hAbuMustafa <hosam5553@gmail.com>
2025-08-01 14:26:40 +0300

    todo

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 
 export function GET({ cookies }) {
+  // todo: delete session data from db first
+
   cookies.delete('session_id', {
     path: '/',
     secure: process.env.NODE_ENV === 'production',
