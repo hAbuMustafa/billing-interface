6838ca1f1fd0a1f0c88219b6a598cccb1a171a79
hAbuMustafa <hosam5553@gmail.com>
2025-08-01 09:11:15 +0300

    re-merge conditionals

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,9 +2,11 @@ import { getSession } from '$lib/server/auth';
 import { redirect } from '@sveltejs/kit';
 
 export async function handle({ event, resolve }) {
-  if (event.route.id?.startsWith('/(authed)')) {
-    event.locals.user = await getSession(event.cookies, event.fetch);
-    if (!event.locals.user) return redirect(303, '/login');
+  if (event.route.id?.startsWith('/(authed)') && !event.locals.user) {
+    const user = await getSession(event.cookies, event.fetch);
+
+    event.locals.user = user;
+    return redirect(303, '/login');
   }
 
   return await resolve(event);
