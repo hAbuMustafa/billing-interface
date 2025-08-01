d8c6792db6737a88b2c10aeab345a2416275b3d4
hAbuMustafa <hosam5553@gmail.com>
2025-08-01 15:40:12 +0300

    implemented the delete endpoint

--- src/routes/api/sheets/delete/+server.ts
+++ src/routes/api/sheets/delete/+server.ts
@@ -1,1 +1,28 @@
-// todo: this should be the Delete in CRUD
+import { deleteRowByCondition } from '../../utils';
+
+export async function POST({ request }) {
+  const body = await request.json();
+  const { spreadsheetId, sheetName, sheetRange, columnIndex, targetValue } = body;
+
+  try {
+    const response = await deleteRowByCondition(
+      spreadsheetId,
+      sheetName,
+      sheetRange,
+      columnIndex,
+      targetValue
+    );
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        response,
+      }),
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return new Response(JSON.stringify({ success: false, error: error.message }), {
+      status: 500,
+    });
+  }
+}
