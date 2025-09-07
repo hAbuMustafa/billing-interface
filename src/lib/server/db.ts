4f39cc5d7abcbb4f0dde752af2b084d85ffe81e4
hAbuMustafa <hosam5553@gmail.com>
2025-09-07 12:44:46 +0300

    use pool for concurrent limitless connections

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,12 +3,16 @@ import { DATABASE_Connection_PW } from '$env/static/private';
 import * as schema from './schema';
 import mysql from 'mysql2/promise';
 
-const connection = await mysql.createConnection({
+const connection = mysql.createPool({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: DATABASE_Connection_PW,
   database: `J23rdHospital${import.meta.env.DEV ? '_TEST' : ''}`,
+
+  connectionLimit: 15,
+  queueLimit: 0,
+  connectTimeout: 60000,
 });
 
 export const db = drizzle({ client: connection, schema, mode: 'default' });
