6de27099358ac12389c691082a1c087d48793302
hAbuMustafa <hosam5553@gmail.com>
2025-09-09 08:03:31 +0300

    basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from '$app/state';
+</script>
+
+<div class="error-page-wrapper" dir="auto" style="text-align: center; font-size:4rem;">
+  <h1>{page.status}</h1>
+  <p>{page.error?.message}</p>
+</div>
