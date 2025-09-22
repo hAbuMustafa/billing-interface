8ca2b21cb5a1e7cd1bb19a18d503db819f20150e
hAbuMustafa <hosam5553@gmail.com>
2025-09-22 17:23:27 +0300

    2 hr sessions only

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -193,7 +193,7 @@ export const Sys_Sessions = sqliteTable(
       .references(() => Sys_Users.id),
     expires_at: integer({ mode: 'timestamp' })
       .notNull()
-      .default(new Date(new Date().getTime() + 60 * 60 * 1000 * 6)),
+      .default(new Date(new Date().getTime() + 60 * 60 * 1000 * 2)),
   },
   (table) => [index('sessions_user_link').on(table.user_id)]
 );
