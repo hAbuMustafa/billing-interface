221385e4031038ec5b1e49dd6ce95fb2570951b3
hAbuMustafa <hosam5553@gmail.com>
2025-09-23 10:22:31 +0300

    starter for people api

---
+++ src/routes/(authed)/api/people/+server.ts
@@ -0,0 +1,5 @@
+export async function POST() {
+  // todo: receive patient name in a request
+  // todo: perform database fuzzy search using extend-arabic-query on `People` table
+  // todo: return id, name, and national_id for every patient
+}
