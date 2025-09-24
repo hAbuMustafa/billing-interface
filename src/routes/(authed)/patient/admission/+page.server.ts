4570bc6ab109e60ab456507fbc05d3c2c76d7eb0
hAbuMustafa <hosam5553@gmail.com>
2025-09-24 14:40:34 +0300

    todo

--- src/routes/(authed)/patient/admission/+page.server.ts
+++ src/routes/(authed)/patient/admission/+page.server.ts
@@ -1,6 +1,7 @@
 import { new_id_doc_type, new_Wards } from '$lib/server/db/menus';
 
 export function load() {
+  // todo: make sure submitted data are not stale when manually changing derived fields from a national id. Or when changing id doc type.
   return {
     title: 'تسجيل دخول مريض',
     id_doc_type_list: new_id_doc_type,
