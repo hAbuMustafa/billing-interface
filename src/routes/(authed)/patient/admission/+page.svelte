7f8c6d63802b7f9fc20d1521b4c0573df1ed2985
hAbuMustafa <hosam5553@gmail.com>
2025-09-25 15:41:59 +0300

    submit button

--- src/routes/(authed)/patient/admission/+page.svelte
+++ src/routes/(authed)/patient/admission/+page.svelte
@@ -24,115 +24,108 @@
   let healthInsurance = $state(0);
 </script>
 
-<main>
-  <form method="POST">
-    <label for="name"> اسم المريض </label>
+<form method="POST">
+  <label for="name"> اسم المريض </label>
+  <input
+    name="name"
+    id="name"
+    type="text"
+    pattern={arabicTetradicNamesPattern.source}
+    required
+  />
+
+  <fieldset>
+    <legend>نوع الهوية</legend>
+    {#each page.data.id_doc_type_list as d_type, i (d_type.id)}
+      <input
+        name="id_doc_type"
+        id="id_doc_type_{i}"
+        type="radio"
+        value={d_type.id}
+        bind:group={idDocType}
+        required
+      />
+      <label for="id_doc_type_{i}">{d_type.name}</label>
+    {/each}
+  </fieldset>
+
+  <label for="id_doc_num">رقم الهوية</label>
+  <input
+    name="id_doc_num"
+    id="id_doc_num"
+    type="text"
+    placeholder="22222222222222"
+    bind:value={idDocNum}
+    pattern={nationalIdPattern.source}
+    required
+  />
+
+  <label for="diagnosis">التشخيص الأولي</label>
+  <input name="diagnosis" id="diagnosis" type="text" />
+  <!-- todo: make this input a form with no bubbling on submit, and create a list of  input:check with name="diagnosis" as the output of this form -->
+
+  <fieldset>
+    <legend>النوع</legend>
+    <input name="gender" id="male" type="radio" value={1} bind:group={gender} required />
+    <label for="male">ذكر</label>
     <input
-      name="name"
-      id="name"
-      type="text"
-      pattern={arabicTetradicNamesPattern.source}
+      name="gender"
+      id="female"
+      type="radio"
+      value={0}
+      bind:group={gender}
       required
     />
+    <label for="female">أنثى</label>
+  </fieldset>
 
-    <fieldset>
-      <legend>نوع الهوية</legend>
-      {#each page.data.id_doc_type_list as d_type, i (d_type.id)}
-        <input
-          name="id_doc_type"
-          id="id_doc_type_{i}"
-          type="radio"
-          value={d_type.id}
-          bind:group={idDocType}
-          required
-        />
-        <label for="id_doc_type_{i}">{d_type.name}</label>
-      {/each}
-    </fieldset>
+  <label for="birthdate">تاريخ الميلاد</label>
+  <input name="birthdate" id="birthdate" type="date" bind:value={birthdate} required />
 
-    <label for="id_doc_num">رقم الهوية</label>
+  <fieldset>
+    <legend>التأمين الصحي</legend>
     <input
-      name="id_doc_num"
-      id="id_doc_num"
-      type="text"
-      placeholder="22222222222222"
-      bind:value={idDocNum}
-      pattern={nationalIdPattern.source}
+      name="health_insurance"
+      id="insured"
+      type="radio"
+      value={1}
+      bind:group={healthInsurance}
       required
     />
-
-    <label for="diagnosis">التشخيص الأولي</label>
-    <input name="diagnosis" id="diagnosis" type="text" />
-    <!-- todo: make this input a form with no bubbling on submit, and create a list of  input:check with name="diagnosis" as the output of this form -->
-
-    <fieldset>
-      <legend>النوع</legend>
-      <input
-        name="gender"
-        id="male"
-        type="radio"
-        value={1}
-        bind:group={gender}
-        required
-      />
-      <label for="male">ذكر</label>
-      <input
-        name="gender"
-        id="female"
-        type="radio"
-        value={0}
-        bind:group={gender}
-        required
-      />
-      <label for="female">أنثى</label>
-    </fieldset>
-
-    <label for="birthdate">تاريخ الميلاد</label>
-    <input name="birthdate" id="birthdate" type="date" bind:value={birthdate} required />
-
-    <fieldset>
-      <legend>التأمين الصحي</legend>
-      <input
-        name="health_insurance"
-        id="insured"
-        type="radio"
-        value={1}
-        bind:group={healthInsurance}
-        required
-      />
-      <label for="insured">مؤمن عليه</label>
-      <input
-        name="health_insurance"
-        id="uninsured"
-        type="radio"
-        value={0}
-        bind:group={healthInsurance}
-        required
-      />
-      <label for="insured">غير مؤمن عليه</label>
-    </fieldset>
-
-    <label for="admission_ward">قسم الدخول</label>
-    <select name="admission_ward" id="admission_ward" required>
-      {#each [{ number: 1, title: 'الرعاية المركزة' }, { number: 2, title: 'الدور الثاني' }, { number: 3, title: 'الدور الثالث' }, { number: 4, title: 'الدور الرابع' }] as floor (floor.number)}
-        <optgroup label={floor.title}>
-          {#each page.data.wards_list.filter((w: { id: number; floor: number; name: string }) => w.floor === floor.number) as ward (ward.id)}
-            <option value={ward.id}>{ward.name}</option>
-          {/each}
-        </optgroup>
-      {/each}
-    </select>
-
-    <label for="admission_date">وقت وتاريخ الدخول</label>
+    <label for="insured">مؤمن عليه</label>
     <input
-      type="datetime-local"
-      name="admission_date"
-      id="admission_date"
-      defaultValue={formatDate(new Date(), 'YYYY-MM-DDTHH:mm')}
+      name="health_insurance"
+      id="uninsured"
+      type="radio"
+      value={0}
+      bind:group={healthInsurance}
       required
     />
-  </form>
-</main>
+    <label for="insured">غير مؤمن عليه</label>
+  </fieldset>
+
+  <label for="admission_ward">قسم الدخول</label>
+  <select name="admission_ward" id="admission_ward" required>
+    {#each [{ number: 1, title: 'الرعاية المركزة' }, { number: 2, title: 'الدور الثاني' }, { number: 3, title: 'الدور الثالث' }, { number: 4, title: 'الدور الرابع' }] as floor (floor.number)}
+      <optgroup label={floor.title}>
+        {#each page.data.wards_list.filter((w: { id: number; floor: number; name: string }) => w.floor === floor.number) as ward (ward.id)}
+          <option value={ward.id}>{ward.name}</option>
+        {/each}
+      </optgroup>
+    {/each}
+  </select>
+
+  <label for="admission_date">وقت وتاريخ الدخول</label>
+  <input
+    type="datetime-local"
+    name="admission_date"
+    id="admission_date"
+    defaultValue={formatDate(new Date(), 'YYYY-MM-DDTHH:mm')}
+    required
+  />
+
+  <input type="submit" value="تسجيل" />
+</form>
 
 <style>
   form {
@@ -150,6 +143,11 @@
 
     input[type='radio'] {
       margin-inline-end: 1rem;
+    }
+
+    input[type='submit'] {
+      margin-block-start: 2rem;
+      padding-block: 0.5rem;
     }
   }
 </style>
