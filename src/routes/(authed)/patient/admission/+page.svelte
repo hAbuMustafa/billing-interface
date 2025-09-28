9016cb53c779cae0a28dd3d5d2d67d5b914d6df1
hAbuMustafa <hosam5553@gmail.com>
2025-09-28 23:25:23 +0300

    simplify admission flow

--- src/routes/(authed)/patient/admission/+page.svelte
+++ src/routes/(authed)/patient/admission/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { page } from '$app/state';
   import { arabicTetradicNamesPattern, nationalIdPattern } from '$lib/stores/patterns';
-  import { formatDate, getAge } from '$lib/utils/date-format';
+  import { formatDate } from '$lib/utils/date-format';
 
   let idDocType = $state(1);
   let idDocNum = $state('');
@@ -22,180 +22,110 @@
     }
   });
   let healthInsurance = $state(0);
-
-  let personText = $state('');
-  let personQuery = $state('');
-
-  let fetchedPeople = $derived(getPeopleByQuery(personQuery) || []);
-
-  async function getPeopleByQuery(
-    query: string
-  ): Promise<{ id: number; name: string; national_id: string; birthdate?: number }[]> {
-    if (query.trim() === '') return [];
-
-    const response = await fetch(`/api/people/?q=${query}`);
-
-    return await response.json();
-  }
 </script>
 
-{#if page.url.searchParams.has('pId') && page.data.person}
-  <form method="POST">
-    <label for="name"> اسم المريض </label>
+<form method="POST">
+  <label for="name"> اسم المريض </label>
+  <input
+    name="name"
+    id="name"
+    type="text"
+    pattern={arabicTetradicNamesPattern.source}
+    required
+  />
+
+  <fieldset>
+    <legend>نوع الهوية</legend>
+    {#each page.data.id_doc_type_list as d_type, i (d_type.id)}
+      <input
+        name="id_doc_type"
+        id="id_doc_type_{i}"
+        type="radio"
+        value={d_type.id}
+        bind:group={idDocType}
+        required
+      />
+      <label for="id_doc_type_{i}">{d_type.name}</label>
+    {/each}
+  </fieldset>
+
+  <label for="id_doc_num">رقم الهوية</label>
+  <input
+    name="id_doc_num"
+    id="id_doc_num"
+    type="text"
+    placeholder="22222222222222"
+    bind:value={idDocNum}
+    pattern={nationalIdPattern.source}
+    required
+  />
+
+  <label for="diagnosis">التشخيص الأولي</label>
+  <input name="diagnosis" id="diagnosis" type="text" />
+  <!-- todo: make this input a form with no bubbling on submit, and create a list of  input:check with name="diagnosis" as the output of this form -->
+
+  <fieldset>
+    <legend>النوع</legend>
+    <input name="gender" id="male" type="radio" value={1} bind:group={gender} required />
+    <label for="male">ذكر</label>
     <input
-      name="name"
-      id="name"
-      type="text"
-      pattern={arabicTetradicNamesPattern.source}
+      name="gender"
+      id="female"
+      type="radio"
+      value={0}
+      bind:group={gender}
       required
     />
+    <label for="female">أنثى</label>
+  </fieldset>
 
-    <fieldset>
-      <legend>نوع الهوية</legend>
-      {#each page.data.id_doc_type_list as d_type, i (d_type.id)}
-        <input
-          name="id_doc_type"
-          id="id_doc_type_{i}"
-          type="radio"
-          value={d_type.id}
-          bind:group={idDocType}
-          required
-        />
-        <label for="id_doc_type_{i}">{d_type.name}</label>
-      {/each}
-    </fieldset>
+  <label for="birthdate">تاريخ الميلاد</label>
+  <input name="birthdate" id="birthdate" type="date" bind:value={birthdate} required />
 
-    <label for="id_doc_num">رقم الهوية</label>
+  <fieldset>
+    <legend>التأمين الصحي</legend>
     <input
-      name="id_doc_num"
-      id="id_doc_num"
-      type="text"
-      placeholder="22222222222222"
-      bind:value={idDocNum}
-      pattern={nationalIdPattern.source}
+      name="health_insurance"
+      id="insured"
+      type="radio"
+      value={1}
+      bind:group={healthInsurance}
       required
     />
-
-    <label for="diagnosis">التشخيص الأولي</label>
-    <input name="diagnosis" id="diagnosis" type="text" />
-    <!-- todo: make this input a form with no bubbling on submit, and create a list of  input:check with name="diagnosis" as the output of this form -->
-
-    <fieldset>
-      <legend>النوع</legend>
-      <input
-        name="gender"
-        id="male"
-        type="radio"
-        value={1}
-        bind:group={gender}
-        required
-      />
-      <label for="male">ذكر</label>
-      <input
-        name="gender"
-        id="female"
-        type="radio"
-        value={0}
-        bind:group={gender}
-        required
-      />
-      <label for="female">أنثى</label>
-    </fieldset>
-
-    <label for="birthdate">تاريخ الميلاد</label>
-    <input name="birthdate" id="birthdate" type="date" bind:value={birthdate} required />
-
-    <fieldset>
-      <legend>التأمين الصحي</legend>
-      <input
-        name="health_insurance"
-        id="insured"
-        type="radio"
-        value={1}
-        bind:group={healthInsurance}
-        required
-      />
-      <label for="insured">مؤمن عليه</label>
-      <input
-        name="health_insurance"
-        id="uninsured"
-        type="radio"
-        value={0}
-        bind:group={healthInsurance}
-        required
-      />
-      <label for="insured">غير مؤمن عليه</label>
-    </fieldset>
-
-    <label for="admission_ward">قسم الدخول</label>
-    <select name="admission_ward" id="admission_ward" required>
-      {#each [{ number: 1, title: 'الرعاية المركزة' }, { number: 2, title: 'الدور الثاني' }, { number: 3, title: 'الدور الثالث' }, { number: 4, title: 'الدور الرابع' }] as floor (floor.number)}
-        <optgroup label={floor.title}>
-          {#each page.data.wards_list.filter((w: { id: number; floor: number; name: string }) => w.floor === floor.number) as ward (ward.id)}
-            <option value={ward.id}>{ward.name}</option>
-          {/each}
-        </optgroup>
-      {/each}
-    </select>
-
-    <label for="admission_date">وقت وتاريخ الدخول</label>
+    <label for="insured">مؤمن عليه</label>
     <input
-      type="datetime-local"
-      name="admission_date"
-      id="admission_date"
-      defaultValue={formatDate(new Date(), 'YYYY-MM-DDTHH:mm')}
+      name="health_insurance"
+      id="uninsured"
+      type="radio"
+      value={0}
+      bind:group={healthInsurance}
       required
     />
+    <label for="insured">غير مؤمن عليه</label>
+  </fieldset>
 
-    <input type="submit" value="تسجيل" />
-  </form>
-{:else}
-  <form
-    onsubmit={(e) => {
-      e.preventDefault();
-      personQuery = personText;
-    }}
-  >
-    <label for="person_query">اسم المريض/رقم الهوية</label>
-    <input
-      type="search"
-      name="person_query"
-      id="person_query"
-      autocomplete="off"
-      bind:value={personText}
-    />
+  <label for="admission_ward">قسم الدخول</label>
+  <select name="admission_ward" id="admission_ward" required>
+    {#each [{ number: 1, title: 'الرعاية المركزة' }, { number: 2, title: 'الدور الثاني' }, { number: 3, title: 'الدور الثالث' }, { number: 4, title: 'الدور الرابع' }] as floor (floor.number)}
+      <optgroup label={floor.title}>
+        {#each page.data.wards_list.filter((w: { id: number; floor: number; name: string }) => w.floor === floor.number) as ward (ward.id)}
+          <option value={ward.id}>{ward.name}</option>
+        {/each}
+      </optgroup>
+    {/each}
+  </select>
 
-    <input type="submit" value="بحث" />
+  <label for="admission_date">وقت وتاريخ الدخول</label>
+  <input
+    type="datetime-local"
+    name="admission_date"
+    id="admission_date"
+    defaultValue={formatDate(new Date(), 'YYYY-MM-DDTHH:mm')}
+    required
+  />
 
-    {#if personQuery}
-      {#await fetchedPeople}
-        <p>جار البحث...</p>
-        <!-- todo: make app wide 'loading...' overlay -->
-      {:then foundPeople}
-        <input type="button" class="new_person_button" value="مريض جديد" />
-
-        {#if foundPeople.length}
-          <div class="person-results">
-            {#each foundPeople as person, i (person.id)}
-              <button class="person-card">
-                <strong>{person.name}</strong>
-                <span class="faded">{person.national_id}</span>
-                {#if person.birthdate}
-                  <span class="birthdate">
-                    {formatDate(new Date(person.birthdate), 'YYYY/MM/DD')}
-                    ({getAge(person.birthdate)} سنة)
-                  </span>
-                  <!-- todo: onclick go to page /admission?pId={this Patient}-->
-                {/if}
-              </button>
-            {/each}
-            <!-- todo: button to add new Person -->
-          </div>
-        {/if}
-      {/await}
-    {/if}
-  </form>
-{/if}
+  <input type="submit" value="تسجيل" />
+</form>
 
 <style>
   form {
@@ -206,7 +136,7 @@
       margin-block-start: 1rem;
     }
 
-    input:is([type='text'], [type*='date'], [type='search']),
+    input:is([type='text'], [type*='date']),
     select {
       font-size: 1.5rem;
     }
@@ -215,31 +145,9 @@
       margin-inline-end: 1rem;
     }
 
-    input:is([type='submit'], [type='button']) {
+    input:is([type='submit']) {
       margin-block-start: 2rem;
       padding-block: 0.5rem;
     }
-  }
-
-  .person-results {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1rem;
-    margin-block-start: 1rem;
-
-    .person-card {
-      display: flex;
-      flex-direction: column;
-      width: 50%;
-
-      .faded {
-        opacity: 0.7;
-      }
-    }
-  }
-
-  .new_person_button {
-    background-color: rebeccapurple;
   }
 </style>
