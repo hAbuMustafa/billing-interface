6f1c6a69ca26ac2a58ff313ff362a045b589565f
hAbuMustafa <hosam5553@gmail.com>
2025-10-05 08:11:32 +0300

    more types

--- src/lib/utils/form-actions.ts
+++ src/lib/utils/form-actions.ts
@@ -1,7 +1,7 @@
 import { fail } from '@sveltejs/kit';
 
 export function failWithFormFieldsAndMessageBuilder<
-  T extends Record<string, string | number | FormDataEntryValue | null>
+  T extends Record<string, string | number | boolean | Date | FormDataEntryValue | null>
 >(fields: T) {
   return (failMessage: string) =>
     fail(401, {
