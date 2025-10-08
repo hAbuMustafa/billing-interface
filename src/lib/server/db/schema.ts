60455a5c7796dd5d4c4a8572b0eb9957bfdd089b
hAbuMustafa <hosam5553@gmail.com>
2025-10-09 00:28:50 +0300

    todo

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -198,7 +198,7 @@ export const Sys_Sessions = sqliteTable(
       .references(() => Sys_Users.id),
     expires_at: integer({ mode: 'timestamp' })
       .notNull()
-      .default(sql`(STRFTIME('%s', 'now', '+2 hours'))`),
+      .default(sql`(STRFTIME('%s', 'now', '+2 hours'))`), // todo: default to the nearest shift time instead
   },
   (table) => [index('sessions_user_link').on(table.user_id)]
 );
