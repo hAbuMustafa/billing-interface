6a25c6be159d1781703388afa6d780bfa1333d1f
hAbuMustafa <hosam5553@gmail.com>
2025-10-12 21:03:10 +0300

    implemented first form action

---
+++ src/routes/(authed)/account/+page.server.ts
@@ -0,0 +1,48 @@
+import { checkIfUnique, updateUser } from '$lib/server/db/operations/users.js';
+import { usernamePattern } from '$lib/stores/patterns.js';
+import { fail } from '@sveltejs/kit';
+
+export function load() {
+  return {
+    title: 'تعديل بيانات الحساب',
+  };
+}
+
+export const actions = {
+  username: async ({ request, locals }) => {
+    const data = await request.formData();
+
+    const username = data.get('username') as unknown as string;
+
+    if (!username) return fail(401, { message: 'اسم المستخدم لا يمكن أن يكون فارغا' });
+
+    if (!usernamePattern.test(username))
+      return fail(401, {
+        message:
+          'صيغة اسم المستخدم غير صحيحة. ينبغي أن تكون حروف انجليزية فقط أو شرطة "-"',
+      });
+
+    if (username === locals.user?.username)
+      return fail(401, { message: 'غيرت إيه انت كدة؟ 🤷🏻‍♂️' });
+
+    const isUnique = await checkIfUnique('username', username);
+
+    if (!isUnique)
+      return fail(401, {
+        message: 'اسم المستخدم يخص مستخدم آخر.',
+      });
+
+    const result = await updateUser(locals.user!.id, { username });
+
+    if (!result.success) return fail(401, { message: 'حدث خطأ غير متوقع.' });
+
+    const oldUsername = locals.user?.username;
+
+    locals.user!.username = username;
+
+    return {
+      success: true,
+      message: `تم تغيير اسم المستخدم من ${oldUsername} إلى ${username}`,
+    };
+  },
+};
