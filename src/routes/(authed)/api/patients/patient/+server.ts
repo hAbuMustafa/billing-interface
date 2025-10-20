6bfdac942cf5b036d12e052eaee604f18fef346c
hAbuMustafa <hosam5553@gmail.com>
2025-10-20 15:15:09 +0300

    gql like return

--- src/routes/(authed)/api/patients/patient/+server.ts
+++ src/routes/(authed)/api/patients/patient/+server.ts
@@ -1,13 +1,6 @@
 import { db } from '$lib/server/db/index.js';
-import {
-  Diagnoses,
-  Patient_Diagnoses,
-  Patient_wards,
-  Patients,
-  People,
-  Wards,
-} from '$lib/server/db/schema';
-import { eq } from 'drizzle-orm';
+import { Patients } from '$lib/server/db/schema';
+import { eq, ne } from 'drizzle-orm';
 
 export async function GET({ url }) {
   const patient_id = url.searchParams.get('id');
@@ -15,15 +8,33 @@ export async function GET({ url }) {
   if (!patient_id || !/^\d{2}\/\d{1,}$/.test(patient_id))
     return new Response('Bad Request', { status: 401 });
 
-  const patient_data = await db
-    .select()
-    .from(Patients)
-    .where(eq(Patients.id, patient_id))
-    .leftJoin(People, eq(Patients.person_id, People.id))
-    .leftJoin(Patient_wards, eq(Patients.id, Patient_wards.patient_id))
-    .leftJoin(Wards, eq(Patient_wards.ward, Wards.id))
-    .leftJoin(Patient_Diagnoses, eq(Patient_Diagnoses.patient_id, patient_id))
-    .leftJoin(Diagnoses, eq(Patient_Diagnoses.diagnosis_id, Diagnoses.id));
+  const patient_data = await db.query.Patients.findFirst({
+    with: {
+      Person: {
+        with: {
+          Patient_id_doc_type: true,
+          Patients: {
+            with: {
+              Patient_discharge_reason: true,
+            },
+            where: ne(Patients.id, patient_id),
+          },
+        },
+      },
+      Patient_wards: {
+        with: {
+          Ward: true,
+        },
+      },
+      Patient_diagnoses: {
+        with: {
+          Diagnosis: true,
+        },
+      },
+      Patient_discharge_reason: true,
+    },
+    where: eq(Patients.id, patient_id),
+  });
 
   return new Response(JSON.stringify(patient_data), {
     headers: {
