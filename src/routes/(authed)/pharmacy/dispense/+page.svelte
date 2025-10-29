4b6bbe50a6a082b16d8d3dc7720af03c40b75cb6
hAbuMustafa <hosam5553@gmail.com>
2025-10-29 13:58:06 +0300

    create patient select page

---
+++ src/routes/(authed)/pharmacy/dispense/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import ISelect from '$lib/components/Forms/iSelect.svelte';
+  import PatientButton from '$lib/components/Forms/PatientButton.svelte';
+
+  type PatientT = {
+    id: string;
+    name: string;
+    id_doc_num: string;
+    admission_date: Date;
+    discharge_date?: Date;
+    recent_ward: number;
+  };
+
+  let patientText = $state('');
+  let selectedPatientId = $state('');
+  let hasSelectedPatient = $state(false);
+</script>
+
+<ISelect
+  endpoint={`/api/patients?q=${patientText}`}
+  bind:value={patientText}
+  bind:selectedValue={selectedPatientId}
+  bind:done={hasSelectedPatient}
+  onclear={() => {
+    hasSelectedPatient = false;
+    selectedPatientId = '';
+  }}
+>
+  {#snippet optionSnippet(patient: PatientT)}
+    <PatientButton {patient} disableIfDischarged={true} />
+  {/snippet}
+</ISelect>
+
+<!-- todo: also add "dispense" column in the patient report page -->
