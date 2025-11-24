b4ad1cf237d665cdc259705c794f4110913784df
hAbuMustafa <hosam5553@gmail.com>
2025-11-24 17:54:51 +0200

    dev db string

--- drizzle-dev.config.ts
+++ drizzle-dev.config.ts
@@ -4,7 +4,11 @@ export default defineConfig({
   dialect: 'mysql',
   schema: './src/lib/server/db/schema/entities',
   out: './drizzle',
-  dbCredentials: { url: process.env.MySQL_Connection_String! },
+  dbCredentials: {
+    url: `${process.env.MySQL_Connection_String!}${
+      process.env.NODE_ENV !== 'production' ? '_TEST' : ''
+    }`,
+  },
   introspect: {
     casing: 'preserve',
   },
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,7 +3,9 @@ import mysql from 'mysql2/promise';
 import { MySQL_Connection_String } from '$env/static/private';
 
 const connection = await mysql.createConnection({
-  uri: MySQL_Connection_String,
+  uri: `${MySQL_Connection_String}${
+    process.env.NODE_ENV !== 'production' ? '_TEST' : ''
+  }`,
 });
 
 export const db = drizzle({
